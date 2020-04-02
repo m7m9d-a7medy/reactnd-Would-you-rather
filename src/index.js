35c582343e5ec00b519503c9f3e4fb03115a6b15
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-04-02 04:01:58 +0200

    feat: Redux devtools activated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './components/App'
@@ -9,8 +9,10 @@ import reducers from './store/reducers'
 import middleware from './store/middleware'
 
 firebaseInit()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, middleware)
+const store = createStore(reducers, composeEnhancers(middleware))
+
 const app = (
     <Provider store={store}>
         <App />
