44b11278203bdd08314dde77230aa866db384b6e
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-04-02 02:06:40 +0200

    feat: Redux connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './components/App'
 import { firebaseInit } from './utils/firebase'
+import reducers from './store/reducers'
+import middleware from './store/middleware'
 
 firebaseInit()
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers, middleware)
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
