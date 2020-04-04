88b7db926555b02c0876245562ca41341e79a39a
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-04-04 05:24:45 +0200

    feat: No duplicate usernames

--- src/utils/authApi.js
+++ src/utils/authApi.js
@@ -2,47 +2,57 @@ import firebase from './firebase'
 
 export const _signUp = (email, password, username, name, avatarURL) => {
     return new Promise((resolve, reject) => {
-        // User is signing up
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(userCredential => {
-                // Successfully created the user
-                // Now uploading the avatar picture
-                const storageRef = firebase.storage().ref()
-                const avatarRef = storageRef.child(`users/${username}/avatar.jpg`)
+        // Check if username exists in the database
+        firebase.firestore().doc(`users/${username}`).get()
+            .then(doc => {
+                if (doc.exists) {
+                    reject('Username exists')
+                } else {
+                    // User is signing up
+                    firebase.auth().createUserWithEmailAndPassword(email, password)
+                        .then(userCredential => {
+                            // Successfully created the user
+                            // Now uploading the avatar picture
+                            const storageRef = firebase.storage().ref()
+                            const avatarRef = storageRef.child(`users/${username}/avatar.jpg`)
 
-                avatarRef.putString(avatarURL, 'data_url')
-                    .then(res => {
-                        // Uploaded the image successfully
-                        // Now getting the url to download it when we need it
-                        res.ref.getDownloadURL()
-                            .then(url => {
-                                // Fetched url correctly
-                                // Done creating the user
-                                // Now creating a firebase document for the user
-                                userCredential.user.updateProfile({
-                                    displayName: username
+                            avatarRef.putString(avatarURL, 'data_url')
+                                .then(res => {
+                                    // Uploaded the image successfully
+                                    // Now getting the url to download it when we need it
+                                    res.ref.getDownloadURL()
+                                        .then(url => {
+                                            // Fetched url correctly
+                                            // Done creating the user
+                                            // Now creating a firebase document for the user
+                                            userCredential.user.updateProfile({
+                                                displayName: username
+                                            })
+                                                .then(() => {
+                                                    const newUser = {
+                                                        id: username,
+                                                        name: name,
+                                                        avatarURL: url,
+                                                        answers: {},
+                                                        questions: []
+                                                    }
+
+                                                    firebase.firestore().doc(`users/${username}`).set(newUser)
+                                                        .then(() => resolve(newUser))
+                                                        .catch(err => reject({ 'Database Error': err }))
+
+                                                })
+                                                .catch(err => reject({ 'User credentials Error': err }))
+                                        })
+                                        .catch(err => reject({ 'Image url fetch Error': err }))
                                 })
-                                    .then(() => {
-                                        const newUser = {
-                                            id: username,
-                                            name: name,
-                                            avatarURL: url,
-                                            answers: {},
-                                            questions: []
-                                        }
-
-                                        firebase.firestore().doc(`users/${username}`).set(newUser)
-                                            .then(() => resolve(newUser))
-                                            .catch(err => reject({ 'Database Error': err }))
-
-                                    })
-                                    .catch(err => reject({ 'User credentials Error': err }))
-                            })
-                            .catch(err => reject({ 'Image url fetch Error': err }))
-                    })
-                    .catch(err => reject({ 'Image upload Error': err }))
+                                .catch(err => reject({ 'Image upload Error': err }))
+                        })
+                        .catch(err => reject({ 'User authentication Error': err }))
+                }
             })
-            .catch(err => reject({ 'User authentication Error': err }))
+            .catch(err => reject({'Username check error': err}))
+
     })
 }
 
