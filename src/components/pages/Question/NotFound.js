6f4f959637a1edf1c519d023a0965361e63b7c7b
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-04-04 09:29:29 +0200

    feat: Not found page

--- src/components/pages/Question/NotFound.js
+++ src/components/pages/Question/NotFound.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = props => {
     return (
-        <div>
-            Not found :'(
+        <div style={{
+            fontSize: '1.8rem',
+            textAlign: 'center'
+        }}>
+            Error 404: Question not found.
+
+            <Link to='/' style={{
+                color: 'black',
+                display: 'block',
+                marginTop: '1rem'
+            }}>
+                Go back
+            </Link>
         </div>
     )
 }
